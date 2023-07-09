1fb536c424f5cc9f2d9c10db9865d80b15884a11
Chetan Choudhary <98541541+AlfieSolomons09@users.noreply.github.com>
2023-07-09 15:36:57 +0530

    Initial Commit

---
+++ TwitterApplication/TwitterApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApplicationApp.swift
+//  TwitterApplication
+//
+//  Created by Chetan Choudhary on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
